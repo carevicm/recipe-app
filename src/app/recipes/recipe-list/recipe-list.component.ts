b04048d406110ae56f1df882262b0b5a2783da0f
Milan <milancarevic@yahoo.com>
2024-04-12 13:03:21 +0200

    first_commit
    recipe_app

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://img.freepik.com/premium-photo/amazing-delicious-cheese-burger_727939-299.jpg?size=338&ext=jpg&ga=GA1.1.1700460183.1712793600&semt=sph'
+    ),
+    new Recipe(
+      'Another Test Recipe',
+      'This is simply a second test',
+      'https://img.freepik.com/free-photo/tasty-burger-isolated-white-background-fresh-hamburger-fastfood-with-beef-cheese_90220-1063.jpg'
+    ),
+  ];
+  constructor() {}
+
+  ngOnInit() {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+   }
+}
