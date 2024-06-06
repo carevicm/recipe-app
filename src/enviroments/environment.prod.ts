9f230d56fa27b1586d8de79307105bb06fb8513d
Milan <milancarevic@yahoo.com>
2024-06-06 11:29:19 +0200

    Remove environments folder from tracking

--- src/enviroments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  firebaseApiKey: 'AIzaSyCNdh4gZFFD8TlyqtKVBrTWeWZX8VPGRyg',
-};
--- src/enviroments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  firebaseApiKey: 'AIzaSyCNdh4gZFFD8TlyqtKVBrTWeWZX8VPGRyg',
-};
